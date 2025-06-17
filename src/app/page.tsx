04efbd29a49c8c6f87a27c1ba0492dcbcb2030fc
Danny <danilomurillo0311@gmail.com>
2025-06-17 10:26:36 +0200

    feat: implement first module to display temperature with bars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import { LineChartsViewer } from '@/components/LineChartsViewer/LineChartsViewer';
+import { fetchTemperatures } from '@/api/forecast';
+import BarTemperatures from '@/components/BarTemperatures/BarTemperatures';
+import { WeatherInterface } from '@/types/temperatures-interface';
 
 export default async function ModuleOne() {
+	const data: WeatherInterface[] = await fetchTemperatures();
+
 	return (
 		<div className='h-screen w-full flex justify-center items-center'>
-			<LineChartsViewer />
+			<div className='max-w-screen w-full'><BarTemperatures data={data}></BarTemperatures></div>
 		</div>
 	);
 }
