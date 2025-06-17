bcd07edd32d35389d743e3afad1f8193386e0e59
Danny <danilomurillo0311@gmail.com>
2025-06-17 09:41:40 +0200

    feat: remove props and set cache rvalidation

--- api/forecast.ts
+++ api/forecast.ts
@@ -1,19 +1,20 @@
-import { OPEN_METEO_URL } from '@/constants/constants';
+import { OPEN_METEO_ENDPOINT } from '@/constants/constants';
 
-export const fetchTemperatures = async ({
-	city,
-}: {
-	city: { lat: number; lon: number };
-}) => {
-	const FORECAST_BASE_URL = `forecast?latitude=${city.lat}&longitude=${city.lon}&minutely_15=temperature_2m&forecast_days=1`;
-	const URL = `${OPEN_METEO_URL}/${FORECAST_BASE_URL}`;
+export const fetchTemperatures = async () => {
+	const config: RequestInit = {
+		method: 'GET',
+		next: {
+			revalidate: 900,
+		},
+	};
 
-	const response = await fetch(URL);
+	const response = await fetch(OPEN_METEO_ENDPOINT, config);
+
 	if (!response.ok) {
+		console.log(response);
 		throw new Error('Failed to fetch OPEN METEO data');
 	}
 
 	const data = await response.json();
 	return data;
 };
-
