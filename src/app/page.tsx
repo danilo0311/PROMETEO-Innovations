5271aacae83ea216f5cd733e1b40a24bb8f89f72
Danny <danilomurillo0311@gmail.com>
2025-06-17 11:32:40 +0200

    feat: implement SpanishMap component and adjust layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import { fetchTemperatures } from '@/api/forecast';
 import BarTemperatures from '@/components/BarTemperatures/BarTemperatures';
+import SpanishMap from '@/components/SpanishMap/SpanishMap';
 import { WeatherInterface } from '@/types/temperatures-interface';
 
 export default async function ModuleOne() {
 	const data: WeatherInterface[] = await fetchTemperatures();
 
 	return (
-		<div className='h-screen w-full flex justify-center items-center'>
-			<div className='max-w-screen w-full'><BarTemperatures data={data}></BarTemperatures></div>
+		<div className='h-screen w-full flex justify-center items-center flex-col gap-y-4'>
+			<div className='h-2/4 w-2/4 bg-gray-100 border-solid border-1 rounded-2xl'>
+				<SpanishMap></SpanishMap>
+			</div>
+			<div className='max-w-[68.5rem] w-full bg-gray-100 border-solid border-1 rounded-2xl'>
+				<BarTemperatures data={data}></BarTemperatures>
+			</div>
 		</div>
 	);
 }
