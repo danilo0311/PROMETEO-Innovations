5625a0be7dc1189f0df5ce12c8bd968c2a0f6442
Danny <danilomurillo0311@gmail.com>
2025-06-17 12:43:28 +0200

    feat: implement Temperature table component and adjust layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,24 @@
 import { fetchTemperatures } from '@/api/forecast';
 import BarTemperatures from '@/components/BarTemperatures/BarTemperatures';
 import SpanishMap from '@/components/SpanishMap/SpanishMap';
+import TemperatureTable from '@/components/TemperatureTable/TemperatureTable';
 import { WeatherInterface } from '@/types/temperatures-interface';
 
 export default async function ModuleOne() {
 	const data: WeatherInterface[] = await fetchTemperatures();
 
 	return (
-		<div className='h-screen w-full flex justify-center items-center flex-col gap-y-4'>
-			<div className='max-w-[68.5rem] w-full bg-gray-100 border-solid border-1 rounded-2xl'>
-				<BarTemperatures data={data}></BarTemperatures>
+		<div className='min-h-screen w-full flex justify-center gap-4 py-16'>
+			<div className='flex w-max max-w-[68.5rem] items-center flex-col gap-y-4'>
+				<div className='w-full bg-gray-100 border-solid border-1 rounded-2xl'>
+					<BarTemperatures data={data}></BarTemperatures>
+				</div>
+				<div className='h-full max-h-[32rem] w-full bg-gray-100 border-solid border-1 rounded-2xl overflow-hidden relative'>
+					<SpanishMap data={data}></SpanishMap>
+				</div>
 			</div>
-			<div className='h-2/4 w-2/4 bg-gray-100 border-solid border-1 rounded-2xl overflow-hidden relative'>
-				<SpanishMap data={data}></SpanishMap>
+			<div className='overflow-y-scroll h-full w-2/4 max-w-[40rem] bg-gray-100 rounded-2xl border-solid border-1 max-h-[40rem]'>
+				<TemperatureTable data={data}></TemperatureTable>
 			</div>
 		</div>
 	);
