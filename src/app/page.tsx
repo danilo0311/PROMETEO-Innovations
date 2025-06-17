027d4e049b3c186a381655044d8319a3fc2cd93e
Danny <danilomurillo0311@gmail.com>
2025-06-17 13:45:51 +0200

    feat: adjust layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,18 @@ export default async function Main() {
 	const data: WeatherInterface[] = await fetchTemperatures();
 
 	return (
-		<div className='min-h-screen w-full flex justify-center gap-4 py-16'>
+		<div className='min-h-screen w-full flex max-[1320px]:items-center justify-center gap-4 py-16 px-8 max-[1320px]:flex-col'>
 			<div className='flex w-full max-w-[68.5rem] items-center flex-col gap-y-4'>
-				<div className='w-full bg-gray-100 border-solid border-1 rounded-2xl'>
-					<BarTemperatures data={data}></BarTemperatures>
+				<div className='w-full overflow-x-scroll bg-gray-100 border-solid border-1 rounded-2xl'>
+					<div className='w-full min-w-2xl'>
+						<BarTemperatures data={data}></BarTemperatures>
+					</div>
 				</div>
 				<div className='h-full max-h-[32rem] w-full bg-gray-100 border-solid border-1 rounded-2xl overflow-hidden relative'>
 					<SpanishMap data={data}></SpanishMap>
 				</div>
 			</div>
-			<div className='overflow-y-scroll h-full w-2/4 max-w-[40rem] bg-gray-100 rounded-2xl border-solid border-1 max-h-[40rem]'>
+			<div className='overflow-y-scroll h-full max-[1320px]:max-w-[68.5rem] max-[1320px]:w-full [1320px]:w-2/4 [1320px]:max-w-[40rem] bg-gray-100 rounded-2xl border-solid border-1 max-h-[40rem]'>
 				<TemperatureTable data={data}></TemperatureTable>
 			</div>
 		</div>
