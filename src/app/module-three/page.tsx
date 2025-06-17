71186f82fb661dd72584f1e0d810ec31cd5c7da9
Danny <danilomurillo0311@gmail.com>
2025-06-17 09:12:47 +0200

    chore:remove pages as everything should be embedded in a single view

--- src/app/module-three/page.tsx
+++
@@ -1,9 +0,0 @@
-import TemperatureTable from '@/components/TemperatureTable/TemperatureTable';
-
-export default function ModuleThree() {
-	return (
-		<div className='text-black flex justify-center min-h-screen w-full p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-			<TemperatureTable></TemperatureTable>
-		</div>
-	);
-}
--- src/app/module-two/page.tsx
+++
@@ -1,9 +0,0 @@
-import { TemperatureMap } from "@/components/TemperaturesMap/MapComponent";
-
-export default async function ModuleTwo() {
-	return (
-		<div className='h-screen w-full flex justify-center items-center'>
-			<TemperatureMap />
-		</div>
-	);
-}
