0c1385a17140bf25b60bf1b194f4e61dfcdcbe4e
Danny <danilomurillo0311@gmail.com>
2025-06-17 10:27:07 +0200

    feat: create interfaces for open meteo data

---
+++ src/types/temperatures-interface.ts
@@ -0,0 +1,33 @@
+export type Minutely15Units = {
+	time: 'iso8601';
+	temperature_2m: '°C';
+};
+
+export type Minutely15Data = {
+	time: string[];
+	temperature_2m: number[];
+};
+
+export interface WeatherInterface {
+	latitude: number;
+	longitude: number;
+	generationtime_ms: number;
+	utc_offset_seconds: number;
+	timezone: string;
+	timezone_abbreviation: string;
+	elevation: number;
+	location_id?: number;
+	minutely_15_units: Minutely15Units;
+	minutely_15: Minutely15Data;
+}
+
+export interface CityFormattedInterface {
+	city: string;
+	time: string[];
+	temperature: number[];
+}
+
+export type BarChartEntry = {
+	time: string;
+	[city: string]: string | number;
+};
